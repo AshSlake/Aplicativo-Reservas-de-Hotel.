1a6d2e994985d10c9f418c1f77f861f208d06c7a
Paulo Henrique <127267437+AshSlake@users.noreply.github.com>
2025-05-20 17:27:23 -0300

    feat(add)✨ Adiciona classe Quarto e implementa navegação de hospedagem
    Implementa a classe `Quarto` com propriedades para descrição e valores diários.
    Atualiza `App.xaml.cs` para inicializar uma lista de quartos.
    Modifica `ContratacaoHospedagem.xaml` para incluir um `Picker` e `DatePicker` para seleção de datas.
    Adiciona lógica em `ContratacaoHospedagem.xaml.cs` para inicialização e navegação.
    Atualiza `HospedagemContratada.xaml` para exibir detalhes da hospedagem e adiciona botão de voltar.
    Implementa lógica de navegação em `HospedagemContratada.xaml.cs`.

--- apphotel/App.xaml.cs
+++ apphotel/App.xaml.cs
@@ -1,13 +1,46 @@
 ﻿
+
+using apphotel.Models;
+
 namespace apphotel
 {
     public partial class App : Application
     {
+        // defini uma lista da Classe Quarto:
+        public List<Quarto> quartos = new List<Quarto>
+        {
+            //criado os objetos quartos a partir da instancia da classe Quarto.
+            new Quarto()
+            {
+                descricao = "SUÍTE SUPER LUXO",
+                valorDiariaAdulto = 110,
+                valorDiariaCrianca = 55
+            },
+            new Quarto()
+            {
+                descricao = "SUÍTE DE LUXO",
+                valorDiariaAdulto = 80,
+                valorDiariaCrianca = 40
+            },
+            new Quarto()
+            {
+                descricao = "SUÍTE DE SINGLE",
+                valorDiariaAdulto = 50,
+                valorDiariaCrianca = 25
+            },
+            new Quarto()
+            {
+                descricao = "SUÍTE DE POBRE",
+                valorDiariaAdulto = 25,
+                valorDiariaCrianca = 10
+            }
+        };
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new Views.SobrePage());
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
--- apphotel/Views/ContratacaoHospedagem.xaml.cs
+++ apphotel/Views/ContratacaoHospedagem.xaml.cs
@@ -1,9 +1,60 @@
 namespace apphotel.Views;
 
+// classe parcial que representa a pagina de contratação de hospedagem da aplicação.
 public partial class ContratacaoHospedagem : ContentPage
 {
-	public ContratacaoHospedagem()
+    // referencia á instância principal do aplicativo, para acessar propriedades globais
+    App propriedadesDoApp;
+
+    public ContratacaoHospedagem()
 	{
-		InitializeComponent();
-	}
+        //inicia os componentes visuais do XAML.
+        InitializeComponent();
+
+        if (Application.Current == null) //Verifica se a instancia da aplicação atual é nula.
+        {
+            throw new Exception("Erro Aplicação nula");
+        }
+
+        //obtem a instância atual da aplicação e faz um cast para a classe do App.
+        propriedadesDoApp = (App)Application.Current;
+
+
+        pck_quarto.ItemsSource = propriedadesDoApp.quartos;// Pega a lista de quartos do picker contido na instancia da aplicação
+
+        // defini a data minima do datePicker
+        dtpck_checkIn.MinimumDate = DateTime.Now;
+        // defini a data maxima do datePicker
+        dtpck_checkIn.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+
+        // Define a data mínima de check-out como um dia após o check-in selecionado
+        dtpck_checkOut.MinimumDate = dtpck_checkIn.Date.AddDays(1);
+        // Define a data máxima de check-out como até 6 meses após o check-in
+        dtpck_checkOut.MaximumDate = dtpck_checkIn.Date.AddMonths(6);
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+        //logica do botão avançar da View de contratação.
+    {
+        try
+            {
+            Navigation.PushAsync(new HospedagemContratada());
+        } catch (Exception ex)
+        {
+
+            DisplayAlert("OPS", ex.Message, "OK");
+        }
+    }
+
+    private void dtpck_checkIn_DateSelected(object sender, DateChangedEventArgs e)
+    // Listenner para a data_CheckIN.
+    // ele altera o minimumDate e maximumDate baseado nas escolha do data_CheckIN.
+    {
+        DatePicker elemento = sender as DatePicker;
+        DateTime data_selecionada = elemento.Date;
+
+        dtpck_checkOut.MinimumDate = data_selecionada.AddDays(1);
+        dtpck_checkOut.MaximumDate = data_selecionada.AddMonths(6);
+
+    }
 }
